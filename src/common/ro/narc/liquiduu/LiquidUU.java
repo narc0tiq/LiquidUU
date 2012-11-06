b8f47028ab6ab766ec6dd3ccea767a9b6d8810e8
Octav Sandulescu <git@narc.ro>
2012-11-06 19:39:27 +0000

    Initial release, v0.5
    Added refinery recipes for cloning water, lava, and fuel.

--- src/common/ro/narc/liquiduu/LiquidUU.java
+++ src/common/ro/narc/liquiduu/LiquidUU.java
@@ -5,16 +5,20 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
+import net.minecraft.src.Block;
 import net.minecraft.src.Item;
 
 import buildcraft.api.liquids.LiquidData;
 import buildcraft.api.liquids.LiquidManager;
 import buildcraft.api.liquids.LiquidStack;
+import buildcraft.api.recipes.RefineryRecipe;
+import buildcraft.BuildCraftEnergy;
 
 import ic2.common.Ic2Items;
 
 @Mod(
         modid="LiquidUU",
+        version="0.5",
         useMetadata=true,
         dependencies="required-after:IC2;required-after:BuildCraft|Transport; required-after:BuildCraft|Energy"
     )
@@ -38,11 +42,13 @@ public class LiquidUU {
         proxy.init(liquidUU);
 
         LiquidStack liquidUUStack = new LiquidStack(liquidUU, 1000);
-
         LiquidData liquidUUData = new LiquidData(liquidUUStack, Ic2Items.matter,
                 Ic2Items.cell);
-
         LiquidManager.liquids.add(liquidUUData);
+
+        RefineryRecipe.registerRefineryRecipe(new RefineryRecipe(new LiquidStack(liquidUU, 1), new LiquidStack(Block.waterStill.blockID, 1), new LiquidStack(Block.waterStill.blockID, 10), 5, 1));
+        RefineryRecipe.registerRefineryRecipe(new RefineryRecipe(new LiquidStack(liquidUU, 1), new LiquidStack(Block.lavaStill.blockID, 1), new LiquidStack(Block.lavaStill.blockID, 5), 5, 1));
+        RefineryRecipe.registerRefineryRecipe(new RefineryRecipe(new LiquidStack(liquidUU, 2), new LiquidStack(BuildCraftEnergy.fuel.shiftedIndex, 1), new LiquidStack(BuildCraftEnergy.fuel.shiftedIndex, 2), 5, 1));
     }
 
 }
