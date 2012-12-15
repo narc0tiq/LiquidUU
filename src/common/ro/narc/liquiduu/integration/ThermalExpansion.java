8daf9144a24a12fd231d026777f12fead85ff16f
Octav Sandulescu <github@narc.ro>
2012-12-15 16:54:30 +0200

    Updated use of TE API

--- src/common/ro/narc/liquiduu/integration/ThermalExpansion.java
+++ src/common/ro/narc/liquiduu/integration/ThermalExpansion.java
@@ -8,9 +8,13 @@ import thermalexpansion.api.crafting.CraftingManagers;
 
 public class ThermalExpansion {
     public static boolean init() {
-        // This should not be reversible (i.e. cannot liquify matter in transposer)
-        CraftingManagers.fillerManager.addRecipe(120,
+        // This should not be reversible (i.e. cannot liquify matter in transposer and get a cell)
+        CraftingManagers.transposerManager.addFillRecipe(120,
             Ic2Items.cell, Ic2Items.matter, LiquidUU.liquidUUStack, false);
+
+        // But do allow liquifying matter in general.
+        CraftingManagers.transposerManager.addExtractionRecipe(120,
+            Ic2Items.matter, null, LiquidUU.liquidUUStack, 0, false);
 
         return true;
     }
