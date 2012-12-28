eebc1119dcc0723d0303dd74cecf880ed15b65ca
Octav Sandulescu <github@narc.ro>
2012-12-28 08:15:11 +0200

    Reduce server-side log spam

--- src/common/ro/narc/liquiduu/integration/NEI.java
+++ src/common/ro/narc/liquiduu/integration/NEI.java
@@ -1,25 +1,32 @@
 package ro.narc.liquiduu.integration;
 
-import java.util.List;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 import codechicken.nei.api.API;
 import codechicken.nei.api.INEIGuiHandler;
 import codechicken.nei.VisiblityData;
 
+import java.util.List;
+
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
 
 import ro.narc.liquiduu.GUIAccelerator;
+import ro.narc.liquiduu.LiquidUU;
 
 public class NEI implements INEIGuiHandler {
     public NEI() {}
 
     public static boolean init() {
-        API.registerNEIGuiHandler((INEIGuiHandler) new NEI());
+        if(LiquidUU.getSide() == Side.CLIENT) {
+            API.registerNEIGuiHandler((INEIGuiHandler) new NEI());
+        }
 
         return true;
     }
 
+    @SideOnly(Side.CLIENT)
     public VisiblityData modifyVisiblity(GuiContainer gui, VisiblityData visibility) {
         if(!(gui instanceof GUIAccelerator)) {
             return visibility;
@@ -32,12 +39,14 @@ public class NEI implements INEIGuiHandler {
         return visibility;
     }
 
+    @SideOnly(Side.CLIENT)
     public int getItemSpawnSlot(GuiContainer gui, ItemStack item) {
         return -1;
     }
 
+    @SideOnly(Side.CLIENT)
     public List getInventoryAreas(GuiContainer gui) {
-      return null;
+        return null;
     }
 
 }
